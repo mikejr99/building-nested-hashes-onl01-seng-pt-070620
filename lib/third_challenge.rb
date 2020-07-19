ae8d965a778c0eb2e6a287b01cfc70af06f9420c
Miguel Felix <mikeyfelix99@gmail.com>
2020-07-19 10:06:05 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -19,19 +19,16 @@ def third_challenge
          {name: "Mercutio", age: "18", attitude: "hot-headed"}
       ]
    }, 
-   :capulet => {
-      :patriarch => {
-        #your key/value pairs here
-        },
-      :matriarch => {
-        #your key/value pairs here
-        },
-      :heroine => {
-        #your key/value pairs here
-        },
-      :heroine_friends => []
+ :capulet => {
+      :patriarch => {name: "Lord Capulet", age: "50"},
+      :matriarch => {name: "Lady Capulet", age: "51"},
+      :heroine => {name: "Juliet", age: "15", status: "alive"},
+      :heroine_friends => [
+          {name: "Steven", age: "30", attitude: "confused"},
+          {name: "Nurse", age: "44", attitude: "worried"}
+      ]
    }
-  }
+}
 
   
 end
